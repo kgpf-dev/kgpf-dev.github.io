5d12ec45dc37f1efc678fa9f99a80d2da16af439
Nils Olsson <nilso@enosis.net>
2023-06-14 20:38:06 -0700

    Update configuration

--- app.vue
+++ app.vue
@@ -1,54 +1,53 @@
 <template>
-  <client-only>
-    <!-- <n-theme-editor> -->
-    <n-config-provider>
-      <n-global-style />
-      <n-message-provider>
-        <n-el
+  <!-- <n-theme-editor> -->
+  <n-config-provider>
+    <n-global-style />
+    <n-message-provider>
+      <n-el
+        :class="[
+          'relative',
+          'min-h-screen',
+          'flex',
+          'flex-col',
+          'items-center',
+        ]"
+      >
+        <n-back-top :right="100" />
+        <div
+          id="header"
           :class="[
-            'relative',
-            'min-h-screen',
+            'py-4',
+            'w-full',
             'flex',
-            'flex-col',
-            'items-center',
+            'justify-center',
+            'bg-hero-topography-white/20',
+            'bg-kgpf-blue',
+            // 'bg-blend-luminosity',
+            'bg-blend-screen',
+            // 'border-b',
           ]"
         >
-          <n-back-top :right="100" />
-          <div
-            id="header"
-            :class="[
-              'py-4',
-              'w-full',
-              'flex',
-              'justify-center',
-              'bg-hero-topography-white/20',
-              'bg-kgpf-blue',
-              // 'bg-blend-luminosity',
-              'bg-blend-screen',
-              // 'border-b',
-            ]"
-          >
-            <div class="relative px-32 flex flex-col sm:flex-row gap-2 sm:gap-8 items-center justify-between">
-              <transition-group name="header">
-                <n-el v-if="!isHome" key="logo" class="left-0">
-                  <nuxt-link to="/" @click="menuSelection = null">
-                    <logo-small />
-                  </nuxt-link>
-                </n-el>
-                <n-menu
-                  key="menu"
-                  v-model:value="menuSelection"
-                  :options="menuOptions"
-                  :dropdown-props="{
-                    showArrow: true,
-                  }"
-                  mode="horizontal"
-                />
-              </transition-group>
-            </div>
+          <div class="relative px-32 flex flex-col sm:flex-row gap-2 sm:gap-8 items-center justify-between">
+            <transition-group name="header">
+              <n-el v-if="!isHome" key="logo" class="left-0">
+                <nuxt-link to="/" @click="menuSelection = null">
+                  <logo-small />
+                </nuxt-link>
+              </n-el>
+              <n-menu
+                key="menu"
+                v-model:value="menuSelection"
+                :options="menuOptions"
+                :dropdown-props="{
+                  showArrow: true,
+                }"
+                mode="horizontal"
+              />
+            </transition-group>
           </div>
-          <div
-            class="
+        </div>
+        <div
+          class="
             flex-1
             w-full
             flex
@@ -56,46 +55,45 @@
             items-center
             px-4
             "
-          >
-            <div class="flex-1 py-8">
-              <nuxt-page />
-            </div>
+        >
+          <div class="flex-1 py-8">
+            <nuxt-page />
           </div>
-          <div id="footer" class="relative p-4 w-full border-t bg-hero-topography-slate-500/10 font-georgia">
-            <!-- <n-divider id="footer-divider"> -->
-            <!--   <div class=""> -->
-            <!--     <img src="/img/logo.png" alt="KGPF logo" class="h-[2cm]"> -->
-            <!--     Kopper & Gruber Public Finance -->
-            <!--   </div> -->
-            <!-- </n-divider> -->
-            <div class="grid grid-cols-3 gap-8 items-center">
-              <n-text :depth="3">
-                <p>334 Via Vera Cruz, Suite 256</p>
-                <p>San Marcos, California 92078</p>
-              </n-text>
-              <n-text :depth="3" class="text-center">
-                <p>Copyright © 2023 Koppel & Gruber Public Finance</p>
-                <p>All Rights Reserved</p>
-              </n-text>
-              <n-text :depth="3" class="text-right">
-                <p>Phone: <a href="tel:7605100290">(760) 510-0290</a></p>
-                <p>Fax: <a href="tel:7605100288">(760) 510-0288</a></p>
-              </n-text>
-            </div>
+        </div>
+        <div id="footer" class="relative p-4 w-full border-t bg-hero-topography-slate-500/10 font-georgia">
+          <!-- <n-divider id="footer-divider"> -->
+          <!--   <div class=""> -->
+          <!--     <img src="/img/logo.png" alt="KGPF logo" class="h-[2cm]"> -->
+          <!--     Kopper & Gruber Public Finance -->
+          <!--   </div> -->
+          <!-- </n-divider> -->
+          <div class="grid grid-cols-3 gap-8 items-center">
+            <n-text :depth="3">
+              <p>334 Via Vera Cruz, Suite 256</p>
+              <p>San Marcos, California 92078</p>
+            </n-text>
+            <n-text :depth="3" class="text-center">
+              <p>Copyright © 2023 Koppel & Gruber Public Finance</p>
+              <p>All Rights Reserved</p>
+            </n-text>
+            <n-text :depth="3" class="text-right">
+              <p>Phone: <a href="tel:7605100290">(760) 510-0290</a></p>
+              <p>Fax: <a href="tel:7605100288">(760) 510-0288</a></p>
+            </n-text>
           </div>
-          <div class="fixed top-0 left-0 p-1 w-[200px] group">
-            <n-slider
-              v-model:value="hueRotate"
-              :min="0"
-              :max="180"
-              class="transition opacity-0 group-hover:opacity-100"
-            />
-          </div>
-        </n-el>
-      </n-message-provider>
-    </n-config-provider>
-    <!-- </n-theme-editor> -->
-  </client-only>
+        </div>
+        <div class="fixed top-0 left-0 p-1 w-[200px] group">
+          <n-slider
+            v-model:value="hueRotate"
+            :min="0"
+            :max="180"
+            class="transition opacity-0 group-hover:opacity-100"
+          />
+        </div>
+      </n-el>
+    </n-message-provider>
+  </n-config-provider>
+  <!-- </n-theme-editor> -->
 </template>
 <script lang="ts" setup>
 import type { MenuOption } from 'naive-ui'
